e3b1c8479fafdafbc008a78873657bc50a6bf4ce
Anne Grantski <youcanreachanne@outlook.com>
2020-09-02 17:27:08 -0700

    add background colors

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const myName = document.getElementById('name-output');
 const myInput = document.getElementById('name-input');
-const myButton = document.getElementById('name-button');
+const nameButton = document.getElementById('name-button');
+const pinkButton = document.getElementById('pink-button');
+const greenButton = document.getElementById('green-button');
+const blueButton = document.getElementById('blue-button');
 
-myButton.addEventListener('click', () => {
+nameButton.addEventListener('click', () => {
     const userInput = myInput.value;
 
     myName.textContent = userInput;
+});
+
+pinkButton.addEventListener('click', () => {
+    document.body.style.background = "pink"
+});
+
+greenButton.addEventListener('click', () => {
+    document.body.style.background = "lightgreen"
+});
+
+blueButton.addEventListener('click', () => {
+    document.body.style.background = "lightblue"
 });
